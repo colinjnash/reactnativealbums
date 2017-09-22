4ba203f5cb20c5fb23015d0cf2b32ab010eade4e
Colin Nash <colinjnash@gmail.com>
2017-09-22 14:43:17 -0400

    Added ScrollView. Image links now working.

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,7 +3,7 @@
 
 
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
@@ -32,9 +32,9 @@ render() {
 
 	return (
 
-		<View>
+		<ScrollView>
 			{this.renderAlbums()}
-		</View>	
+		</ScrollView>	
 		);
 }
 
