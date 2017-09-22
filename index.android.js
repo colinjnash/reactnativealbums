1d4a4f6fbb3e215f5ce232128d6bac2fe6237d2d
Colin Nash <colinjnash@gmail.com>
2017-09-22 14:57:30 -0400

    Flex:1 ScrollView

--- index.android.js
+++ index.android.js
@@ -7,8 +7,10 @@ import AlbumList from './src/components/AlbumList';
 
 // Create a component
 
-const App = () => ( <View>
-  <Header headerText={'Albums'} />
+const App = () => ( 
+
+  <View style={{ flex: 1}}>
+    <Header headerText={'Albums'} />
   <AlbumList />
   </View>
   );
